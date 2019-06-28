89055cd28c9031c0acb801de953b50ccef2479a1
Jean Connelly <jconnelly@cfa.harvard.edu>
2019-06-28 09:54:43 -0400

    Put all the parameters in the function definition

--- yoshi/yoshi.py
+++ yoshi/yoshi.py
@@ -5,7 +5,11 @@ from chandra_aca.drift import get_aca_offsets
 import proseco
 
 
-def run_one_yoshi(**kw):
+def run_one_yoshi(*, detector, chipx, chipy, chip_id,
+                  ra_targ, dec_targ, roll_targ,
+                  offset_y, offset_z, sim_offset, focus_offset,
+                  dither_y, dither_z,
+                  obs_date, t_ccd, man_angle):
     """
     Run proseco and sparkles for an observation request in a roll/temperature/man_angle
     scenario.
@@ -33,28 +37,28 @@ def run_one_yoshi(**kw):
     """
 
     # Calculate dynamic offsets using the supplied temperature.
-    aca_offset_y, aca_offset_z = get_aca_offsets(kw['detector'],
-                                                 kw['chip_id'], kw['chipx'], kw['chipy'],
-                                                 kw['obs_date'], kw['t_ccd'])
+    aca_offset_y, aca_offset_z = get_aca_offsets(detector,
+                                                 chip_id, chipx, chipy,
+                                                 obs_date, t_ccd)
 
     # Get the ACA quaternion using target offsets and dynamic offsets.
     # Note that calc_aca_from_targ expects target offsets in degrees and obs is now in arcmin
-    q_pnt = calc_aca_from_targ((kw['ra_targ'], kw['dec_targ'], kw['roll_targ']),
-                               (kw['offset_y'] / 60.) + (aca_offset_y / 3600.),
-                               (kw['offset_z'] / 60.) + (aca_offset_z / 3600.))
+    q_pnt = calc_aca_from_targ((ra_targ, dec_targ, roll_targ),
+                               (offset_y / 60.) + (aca_offset_y / 3600.),
+                               (offset_z / 60.) + (aca_offset_z / 3600.))
 
     # Run proseco and sparkles
     aca = proseco.get_aca_catalog(obsid=0,
                                   att=q_pnt,
-                                  man_angle=kw['man_angle'],
-                                  date=kw['obs_date'],
-                                  t_ccd_acq=kw['t_ccd'],
-                                  t_ccd_guide=kw['t_ccd'],
-                                  dither_acq=(kw['dither_y'], kw['dither_z']),
-                                  dither_guide=(kw['dither_y'], kw['dither_z']),
-                                  detector=kw['detector'],
-                                  sim_offset=kw['sim_offset'],
-                                  focus_offset=kw['focus_offset'],
+                                  man_angle=man_angle,
+                                  date=obs_date,
+                                  t_ccd_acq=t_ccd,
+                                  t_ccd_guide=t_ccd,
+                                  dither_acq=(dither_y, dither_z),
+                                  dither_guide=(dither_y, dither_z),
+                                  detector=detector,
+                                  sim_offset=sim_offset,
+                                  focus_offset=focus_offset,
                                   n_acq=8, n_guide=5, n_fid=3)
     acar = aca.get_review_table()
     acar.run_aca_review()
